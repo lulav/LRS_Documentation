557b6d2111df78b4f3fa9e5dd3eb93eaaf5bd7e8
vovacooper <vovacooper@gmail.com>
2023-09-22 09:46:36 +0300

    please dont change!

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,10 +116,10 @@ const config = {
 
   stylesheets: [
     {
-      href: "katex/katex.min.css",
-      type: "text/css",
-      // href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      // type: 'text/css',
+    //   href: "katex/katex.min.css",
+    //   type: "text/css",
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
       // integrity:
       //   'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
       // crossorigin: 'anonymous',
